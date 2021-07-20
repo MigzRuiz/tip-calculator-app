5a5fe34ca98971ca729de14f3c8812f2f0858bad
Miguel Ruiz <migz.p.ruiz@gmail.com>
2021-07-19 21:10:49 -0700

    Adding new js code

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,10 @@
 console.log("hello world");
+
+//VARIABLES
+const input = document.querySelector(".input__bill");
+const log = document.getElementById("log");
+
+input.addEventListener("change", (e) => {
+  e.preventDefault();
+  console.log(e.target.value);
+});
